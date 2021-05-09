a7c71f37d25ae0c58729eec0cc44c98bf40e7f05
jdeburgo <jpdeburgo.jp@gmail.com>
2021-05-09 10:45:33 -0400

    adding infographic

--- build-table.js
+++ build-table.js
@@ -1,7 +1,11 @@
 let sources = [
-    { name: "UMD", description: "site desc", link: "umd.edu" },
-    { name: "hi",  description: "nice", link: "hi.edu" },
-    { name: "Bowie", description: "virtual", link: "Bowie.edu" }
+    { name: "Facebook", description: "Social media", link: "facebook.com" },
+    { name: "Instagram", description: "Social media", link: "instagram.com" },
+    { name: "Discord", description: "Virtual messaging and video chat platform", link: "discord.com" },
+    { name: "Zoom", description: "Video chatting platform", link: "zoom.com" },
+    { name: "TerpLink", description: "A site for UMD students and alumni to find oppurtunities", link: "terplink.umd.edu" },
+    { name: "Society of Physics Students", description: "The society of physics students", link: "sps.physics.umd.edu" },
+    { name: "Student Organization Resources Center", description: "The student orginization resource center at UMD", link: "thestamp.umd.edu/student_org_resource_center_sorc" },
 ];
 
 function generateTableHead(table, data) {
@@ -20,18 +24,27 @@ function generateTable(table, data) {
         let row = table.insertRow();
         for (key in element) {
             let cell = row.insertCell();
+            let a = document.createElement('a');
             let text = document.createTextNode(element[key]);
-            cell.appendChild(text);
+            a.appendChild(text);
+            a.title = element[key];
+            a.href = "https://"+element[key];
+            if (key == "link") {
+                cell.appendChild(a);
+            }
+            else {
+                cell.appendChild(text);
+            }
         }
     }
 }
 
 function filterTable() {
     let search = document.getElementById("search").value
-    let filteredSources = sources.filter(source => source.name.includes(search.toLowerCase()) || source.description.includes(search.toLowerCase()) || source.link.includes(search.toLowerCase()))
+    let filteredSources = sources.filter(source => source.name.toLowerCase().includes(search.toLowerCase()) || source.description.toLowerCase().includes(search.toLowerCase()) || source.link.toLowerCase().includes(search.toLowerCase()))
     let table = document.querySelector("table");
     table.removeChild(table.children[0])
-    let data = Object.keys(filteredSources[0]);
+    let data = Object.keys(sources[0]);
     generateTableHead(table, data);
     generateTable(table, filteredSources);
 }
